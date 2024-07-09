e8fe8bf8a90435aa72b6e8bb71e281b70534de00
Oscar A. Monroy <oscara.monroym@ecci.edu.co>
2024-07-09 17:19:02 -0500

    direccion render

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports ={
     api:{
-        baseURL:"http://localhost:5000"
+        // baseURL:"http://localhost:5000"
+        baseURL:"https://backendg80p.onrender.com"
+        
     }
 }
